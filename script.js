5972500afe4e1ad436a9d2345d5ffedd8ceb4afd
Ilse Ramirez <ilsetacho476@gmail.com>
2025-08-22 12:02:21 -0600

    Changing the piano setup

--- script.js
+++ script.js
@@ -34,3 +34,48 @@ slider.addEventListener("input", function(){
     osc.frequency.setTargetAtTime (slider.value, now, 0.5);
 
 });
+// Seleccionar teclas
+const keys = document.querySelectorAll(".white_key, .black_key");
+keys.forEach (key => { // Click en teclas
+    key.addEventListener("click", () => {
+        const now = ctx.currentTime;
+        const freq = parseFloat(key.dataset.note);
+        osc.frequency.setTargetAtTime(freq, now, 0.1);
+
+    // Feedback visual
+        key.classList.add("active");
+        setTimeout(() => key.classList.remove("active"), 200);
+
+    });
+});
+// Mapeo teclado físico
+const keyMap = {
+    'a': 261.63, //Do
+    'w': 277.18,  //Do#
+    's': 293.66, //Re
+    'e': 311.13, //Re#
+    'd': 329.63,  //Mi
+    'f': 349.23, //Fa
+    'r': 369.99, //Fa#
+    'g': 392.00, //Sol
+    't': 415.30, //Sol#
+    'h': 440.00, //La
+    'y': 466.16, //La#
+    'j':493.88 //Si
+            
+};
+// Escuchar teclado físico
+document.addEventListener("keydown", (event) =>{
+    const key = event.key.toLowerCase();
+    if (keyMap[key]){
+        const now = ctx.currentTime;
+        osc.frequency.setTargetAtTime(keyMap[key], now, 0.1);
+
+        // Feedback visual de la tecla correspondiente
+        const htmlKey = document.querySelector (`[data-note="${keyMap[key]}"]`);
+        if(htmlKey){
+            htmlKey.classList.add("active");
+            setTimeout(()=> htmlKey.classList.remove("active"), 200);
+        }
+    }
+});
